54890b50512dc959c89bf5912706c78dcf7c0a36
Arjun Fifield <90934514+ArjunFifield@users.noreply.github.com>
2024-09-25 07:23:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1390,4 +1390,7 @@ def send_market_order_gap(quantity, symbol, sl_gap=None, tp_gap=None):
         print(error_msg)
         return error_msg
 
-
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
